11c08e93dba8dafa6aa8309173f9a128b1b09bdc
Kongxib Vue <kongcvue@gmail.com>
2021-10-22 16:42:30 -0500

    imported photos and installed neccessary libraries

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,39 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, 
+    path: 'images/goat_small.jpg', 
+    description: 'Photo of a goat taken at Glacier National Park.', 
+    likes: 0 },
+
+    {id: 2, 
+    path: 'images/boston.jpg', 
+    description: 'On my free time, I love exploring the outdoors as that is where I find peace with many things in my life', 
+    likes: 0},
+
+    {id: 3, 
+    path: 'images/finland.jpg', 
+    description: 'My brother runs behind our campsite for me to capture him in the best of light rays', 
+    likes: 0},
+
+    {id: 4, 
+    path: 'images/polaroid.jpg', 
+    description: 'Last picture with my younger brother before he heads out to LA', 
+    likes: 0},
+
+    {id: 5, 
+    path: 'images/popslaab.jpg', 
+    description: "If you know my father, you know he's known for his famous laab!", 
+    likes: 0},
+
+    {id: 6, 
+    path: 'images/rainytents.jpg', 
+    description: 'Our last night having dinner inside our tents as the rain begins to pour...', 
+    likes: 0},
+
+    {id: 7, 
+    path: 'images/sweetie.jpg', 
+    description: "My truly wonderful sweetie, isn't she lovely? <3 ", 
+    likes: 0},
+
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, (useEffect) from 'react';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+    fetchPhotos();
+  })
+
+  //GET route
+
+
+
+  //
+
+  console.log(galleryItems);
     return (
       <div className="App">
         <header className="App-header">
@@ -9,6 +23,7 @@ function App() {
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <img width="100" height="100 "src="images/sweetie.jpg"/>
       </div>
     );
 }
